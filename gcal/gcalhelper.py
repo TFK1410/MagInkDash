f491da14d32d58baf410ef1b09a1db9aa32b5b48
TFK1410 <tfk1410@gmail.com>
2023-07-03 19:29:05 +0200

    Modified the path to the token to accomodate the mounting of it

--- gcal/gcalhelper.py
+++ gcal/gcalhelper.py
@@ -28,8 +28,8 @@ class GcalHelper:
         # The file token.pickle stores the user's access and refresh tokens, and is
         # created automatically when the authorization flow completes for the first
         # time.
-        if os.path.exists(self.currPath + '/token.pickle'):
-            with open(self.currPath + '/token.pickle', 'rb') as token:
+        if os.path.exists(self.currPath + '/token/token.pickle'):
+            with open(self.currPath + '/token/token.pickle', 'rb') as token:
                 creds = pickle.load(token)
         # If there are no (valid) credentials available, let the user log in.
         if not creds or not creds.valid:
@@ -40,7 +40,7 @@ class GcalHelper:
                     self.currPath + '/credentials.json', SCOPES)
                 creds = flow.run_local_server(port=0)
             # Save the credentials for the next run
-            with open(self.currPath + '/token.pickle', 'wb') as token:
+            with open(self.currPath + '/token/token.pickle', 'wb') as token:
                 pickle.dump(creds, token)
 
         self.service = build('calendar', 'v3', credentials=creds, cache_discovery=False)
