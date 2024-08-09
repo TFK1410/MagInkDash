495147b11ee9948671dd37b279fd435ea536184e
Pawel Rutkowski <tfk1410@gmail.com>
2024-08-09 13:52:37 +0200

    Add debug print

--- main.py
+++ main.py
@@ -24,6 +24,7 @@ import socket
 old_getaddrinfo = socket.getaddrinfo
 def new_getaddrinfo(*args, **kwargs):
     responses = old_getaddrinfo(*args, **kwargs)
+    print(responses)
     return [response
         for response in responses
         if response[0] == socket.AF_INET]
